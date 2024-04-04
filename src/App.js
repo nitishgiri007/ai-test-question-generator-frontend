148aa9a53da84aa325b001374414f1e480706eb5
Nitish Bharti <nb88640@gmail.com>
2024-04-05 01:48:55 +0530

    new changes done

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Navigation from "./components/dashboard/navbar/navigation";
+import Content from "./components/dashboard/content/content";
+import FooterPage from "./components/dashboard/footer/footer";
+import SignUp from "./components/signuUP/signupComponst";
+
+const Layout = () => {
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+      <FooterPage />
+    </>
+  );
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Content />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <div>About Page</div>,
+  },
+  
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/dashboard/content/content.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200" data-theme="dracula">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Want to Generate Qusetions</h1>
+            <p className="py-6">
+              Use our Service in order to Generate MCQs Qusetions Easily !!!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Content;
